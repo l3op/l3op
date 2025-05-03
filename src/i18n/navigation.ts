5d778020fd4c4ef846f17599fdff3320b43e157f
Lucas <c8li@hotmail.com>
2025-05-03 22:57:39 +0300

    Fix i18n.

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -1,4 +1,4 @@
 import { routing } from "@/i18n/routing";
 import { createNavigation } from "next-intl/navigation";
 
-export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
+export const { Link, usePathname, useRouter, getPathname, redirect, permanentRedirect } = createNavigation(routing);
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -5,7 +5,6 @@ import { getRequestConfig } from "next-intl/server";
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
-
   return {
     locale,
     messages: (await import(`@/locales/${locale}.json`)).default,
