c6007f6baff020ef1f9f29d7999c78891e844c66
Lucas <c8li@hotmail.com>
2025-05-03 22:55:42 +0300

    Update not found page.

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -2,12 +2,14 @@ import { Link } from "@/i18n/navigation";
 import { getTranslations } from "next-intl/server";
 
 export default async function LocaleNotFound() {
-  const t = await getTranslations('NotFoundPage');
-
+  const t = await getTranslations('Pages.NotFoundPage');
   return (
-    <div className="mb-40 mt-52 flex flex-col items-center justify-center gap-12">
-      <h1>{t("title")}</h1>
-      <Link href="/">{t("backToHome")}</Link>
-    </div>
+    <main className="flex h-[calc(100svh-5rem)] flex-col items-center justify-center p-2 border border-dashed rounded-md">
+      <title>{t('meta.title')}</title>
+      <div className="relative text-center max-w-md">
+        <h1 className="text-foreground mt-4 text-xl font-bold tracking-tight xl:text-2xl md">{`${t('title')} :(`}</h1>
+        <Link href="/">{t("backToHome")}</Link>
+      </div>
+    </main>
   );
 }
