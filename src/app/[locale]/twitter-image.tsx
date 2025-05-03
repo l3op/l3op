cd25ae6cf43d32d3b5fa3476684b3dba39c2051a
Lucas <c8li@hotmail.com>
2025-05-03 22:56:20 +0300

    Remove twitter.

--- src/app/[locale]/twitter-image.tsx
+++
@@ -1,8 +0,0 @@
-import { ImageResponse } from 'next/og';
-import { getTranslations } from 'next-intl/server';
-
-export default async function LocaleTwitterImage({ params }: { params: Promise<{ locale: string }> }) {
-    const { locale } = await params;
-    const t = await getTranslations({ locale, namespace: 'TwitterImage' });
-    return new ImageResponse(<div style={{ fontSize: 128 }}>{t('title')}</div>);
-}
