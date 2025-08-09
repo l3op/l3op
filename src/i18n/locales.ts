56c5f118d913d15ecb3f20d43684d195a9250785
Lucas <c8li@hotmail.com>
2025-08-09 06:37:41 +0300

    [I18n] Define locales type.

--- src/i18n/locales.ts
+++ src/i18n/locales.ts
@@ -1,4 +1,11 @@
-export const locales = [
+interface locale {
+  lang: string;
+  name: string;
+  dir: string;
+  flag: string;
+}
+
+export const locales: locale[] = [
   { lang: 'ar', name: 'العربية', dir: 'rtl', flag: '🇸🇦' },
   { lang: 'en', name: 'English', dir: 'ltr', flag: '🇺🇸' }
 ]
