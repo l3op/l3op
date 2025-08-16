91825e25ade186735bf87fa1a48d1ff121aeca62
Lucas <c8li@hotmail.com>
2025-08-16 06:02:09 +0300

    [About] Fixing metada of about page.

--- src/app/[locale]/about-me/page.tsx
+++ src/app/[locale]/about-me/page.tsx
@@ -1,7 +1,8 @@
+import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import { LocaleProps } from "@/lib/types";
 
-export async function generateMetadata({ params }: LocaleProps) {
+export async function generateMetadata({ params }: LocaleProps): Promise<Metadata> {
   const { locale } = await params;
   const t = await getTranslations({ locale, namespace: 'Pages.AboutMePage' });
   return { title: t('meta.title'), description: t('meta.description') };
